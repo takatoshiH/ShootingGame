68209dcf66e06fe3b843598a260af558a7e7e1e2
takatoshiH <t.hikijiri@gmail.com>
2020-01-03 15:07:23 +0900

    イロイロ

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-target = document.getElementsByClassName('target');
-console.log(target);
+field = document.getElementById("field");
+console.log(window.getComputedStyle(field).width);
+
+function createTarget() {
+  var target = document.createElement('div');
+  target.id = "target";
+  field.appendChild(target);
+  console.log("できた?");
+}
+
+createTarget();
+
+// target = document.getElementById("target");
+// setInterval(() => {
+//   target.style.marginLeft = "20%";
+// }, 1000);
