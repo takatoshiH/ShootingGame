7b03e1115c3de55cc2cf9a8cf41038d12943952a
takatoshiH <t.hikijiri@gmail.com>
2020-01-06 01:15:45 +0900

    修正

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 class Target {
 
   constructor() {
-    this.marginLeft = 0;
-    this.marginTop = Math.random() * 100;
+    this.left = 0;
+    this.top = Math.random() * 100;
     this.speed = Math.random() * 2000;
     this.target = document.getElementById("field").appendChild(document.createElement('div'));
     this.target.classList.add('target');
-    this.target.style.marginLeft = String(this.marginLeft) + "px";
-    this.target.style.marginTop = String(this.marginTop) + "px";
+    this.target.style.left = String(this.left) + "px";
+    this.target.style.top = String(this.top) + "px";
 
     this.interval_id = setInterval(() => {
-      var new_margin = parseInt(window.getComputedStyle(field).width) / this.speed + this.marginLeft;
+      var new_margin = parseInt(window.getComputedStyle(field).width) / this.speed + this.left;
       if (new_margin > window.parent.screen.width * 1.1) {
         clearInterval(this.interval_id);
       } else {
-        this.marginLeft = new_margin;
-        this.target.style.marginLeft = String(this.marginLeft) + "px";
+        this.left = new_margin;
+        this.target.style.left = String(this.left) + "px";
       }
     }, 10);
   }
@@ -33,9 +33,17 @@ class bullet {
     this.top = parseInt(window.getComputedStyle(field).height) * 0.95;
     this.bullet.style.left = String(this.left) + "px";
     this.bullet.style.top = String(this.top) + "px";
+
+    this.interval_id = setInterval(() => {
+      this.top -= 1;
+      this.bullet.style.top = String(this.top) + "px";
+    }, 10);
   }
 }
 
+target = new Target();
+target = new Target();
+target = new Target();
 target = new Target();
 bullet = new bullet();
 
